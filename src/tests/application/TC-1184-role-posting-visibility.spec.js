8f46fd8247c65e6eed3ee5917f39af2988424eda
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-06-16 01:01:51 +0530

    Update TC-1184-role-posting-visibility.spec.js

--- src/tests/application/TC-1184-role-posting-visibility.spec.js
+++ src/tests/application/TC-1184-role-posting-visibility.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('../../fixtures');
+const { test, expect } = require('@playwright/test');
 const CoreAskPage = require('../../pages/core-ask.page');
 const TD = require('../../data/core-ask-test-data');
 
